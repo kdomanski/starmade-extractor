b7d5871bc015cbffa692aec3d251985e428338ec
Kamil Domanski <kdomanski@kdemail.net>
2014-04-20 09:10:26 +0200

    use class loader

--- StarMadeExtractor.java
+++ StarMadeExtractor.java
@@ -1,17 +1,24 @@
-import org.schema.game.network.objects.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URLClassLoader;
+import java.net.URL;
 import java.util.Arrays;
-import java.util.Iterator;
 
 public class StarMadeExtractor {
 
 	public static void main(String[] argv) {
 		String classes[] = {"org.schema.game.network.objects.NetworkShip"};
+		
+		try {
+			File file = new File(argv[0]);
+			if( !file.exists() )
+				throw new FileNotFoundException(file.getPath());
+			URLClassLoader loader = new URLClassLoader (new URL[] { file.toURI().toURL() }, StarMadeExtractor.class.getClassLoader());
 
-		for (String cname : classes) {
-			try {
-				Class<?> c = Class.forName(cname);
+			for (String cname : classes) {
+				Class<?> c = Class.forName(cname, true, loader);
 				String[] fields = new String[c.getFields().length];
 				for (int i=0; i<fields.length; i++) {
 					fields[i] = c.getFields()[i].getName();
@@ -26,9 +33,12 @@ public class StarMadeExtractor {
 				}
 				System.out.println("-" + c.getSimpleName());
 			}
+		}
 			catch (ClassNotFoundException x) {
-				x.printStackTrace();
-			}
+			x.printStackTrace();
+		}
+			catch (IOException x) {
+			x.printStackTrace();
 		}
 	}
 }
