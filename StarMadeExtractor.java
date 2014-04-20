26cfcf00b08181a1b91f128482b60ab4716bba42
Kamil Domanski <kdomanski@kdemail.net>
2014-04-20 16:28:34 +0200

    separate functions

--- StarMadeExtractor.java
+++ StarMadeExtractor.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
 import java.net.URLClassLoader;
 import java.net.URL;
 import java.util.Arrays;
@@ -10,34 +11,52 @@ public class StarMadeExtractor {
 
 	public static void main(String[] argv) {
 		String classes[] = {"org.schema.game.network.objects.NetworkShip"};
+		URLClassLoader loader = getLoader(argv[0]);
+
+		for (String cname : classes) {
+			printClass(loader, cname);
+		}
 		
+	}
+
+	private static URLClassLoader getLoader(String path) {
 		try {
-			File file = new File(argv[0]);
+			File file = new File(path);
+			
 			if( !file.exists() )
 				throw new FileNotFoundException(file.getPath());
+
 			URLClassLoader loader = new URLClassLoader (new URL[] { file.toURI().toURL() }, StarMadeExtractor.class.getClassLoader());
+			return loader;
+		} catch (FileNotFoundException x) {
+			x.printStackTrace();
+			System.exit(1);
+			return null;
+		} catch (MalformedURLException x) {
+			x.printStackTrace();
+			System.exit(1);
+			return null;
+		}
+	}
 
-			for (String cname : classes) {
-				Class<?> c = Class.forName(cname, true, loader);
-				String[] fields = new String[c.getFields().length];
-				for (int i=0; i<fields.length; i++) {
-					fields[i] = c.getFields()[i].getName();
-				}
-
-				assert (fields.length < 127);
-				Arrays.sort(fields);
-
-				System.out.println("+" + c.getSimpleName());
-				for (String str : fields) {
-					System.out.println(str);
-				}
-				System.out.println("-" + c.getSimpleName());
+	private static void printClass(URLClassLoader loader, String name) {
+		try {
+			Class<?> c = Class.forName(name, true, loader);
+			String[] fields = new String[c.getFields().length];
+			for (int i=0; i<fields.length; i++) {
+				fields[i] = c.getFields()[i].getName();
 			}
+
+			assert (fields.length < 127);
+			Arrays.sort(fields);
+
+			System.out.println("+" + c.getSimpleName());
+			for (String str : fields) {
+				System.out.println(str);
+			}
+			System.out.println("-" + c.getSimpleName());
 		}
 			catch (ClassNotFoundException x) {
-			x.printStackTrace();
-		}
-			catch (IOException x) {
 			x.printStackTrace();
 		}
 	}
