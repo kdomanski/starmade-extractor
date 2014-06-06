409f880bc0adf2e62c2c80393e0938529b175bc9
Kamil Domanski <kdomanski@kdemail.net>
2014-06-06 06:18:24 +0200

    NetworkSegmentProvider

--- StarMadeExtractor.java
+++ StarMadeExtractor.java
@@ -16,7 +16,8 @@ public class StarMadeExtractor {
 			"org.schema.game.network.objects.NetworkShop",
 			"org.schema.game.network.objects.NetworkSector",
 			"org.schema.game.network.objects.NetworkPlayer",
-			"org.schema.game.network.objects.NetworkSpaceStation"
+			"org.schema.game.network.objects.NetworkSpaceStation",
+			"org.schema.game.network.objects.NetworkSegmentProvider"
 		};
 		URLClassLoader loader = getLoader(argv[0]);
 
